189ec90d3e0c947cc24af0e50cdba80656664200
Juan Pablo Camejo <jp.came@gmail.com>
2016-09-10 11:12:11 -0300

    Se agrega el OutingTag

---
+++ desapp-grupoe-backend/src/main/java/model/OutingTag.java
@@ -0,0 +1,52 @@
+package model;
+
+public class OutingTag {
+    private long id;
+    private String name;
+    private OutingTag parent;
+
+    public OutingTag(long id, String name, OutingTag parent) {
+        this.id = id;
+        this.name = name;
+        this.parent = parent;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public OutingTag getParent() {
+        return parent;
+    }
+
+    public void setParent(OutingTag parent) {
+        this.parent = parent;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OutingTag outingTag = (OutingTag) o;
+        return id == outingTag.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (id ^ (id >>> 32));
+    }
+
+
+}
