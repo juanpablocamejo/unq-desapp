1943666169448b29af72dcbeca2476fcf85a601d
Juan Pablo Camejo <jp.came@gmail.com>
2016-09-12 02:21:25 -0300

    Se corrigen los modificadores de acceso y se agrega accessors.

--- desapp-grupoe-backend/src/main/java/model/Application.java
+++ desapp-grupoe-backend/src/main/java/model/Application.java
@@ -8,7 +8,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
-    List<User> users;
-    List<OutingPlace> places = new ArrayList<>();
-    List<OutingEvent> events = new ArrayList<>();
+    private List<User> users;
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    public List<OutingPlace> getPlaces() {
+        return places;
+    }
+
+    public void setPlaces(List<OutingPlace> places) {
+        this.places = places;
+    }
+
+    public List<OutingEvent> getEvents() {
+        return events;
+    }
+
+    public void setEvents(List<OutingEvent> events) {
+        this.events = events;
+    }
+
+    private List<OutingPlace> places = new ArrayList<>();
+    private List<OutingEvent> events = new ArrayList<>();
 }
