9a0afb3a455ff5ed93b8b61320d6913d82389cc1
Esteban <esteban.schafir@gmail.com>
2016-09-17 17:51:18 -0300

    Refactoring de findOutings. Falta contemplar los OutingEvents y la posibilidad de recibir null

--- desapp-grupoe-backend/src/main/java/model/Application.java
+++ desapp-grupoe-backend/src/main/java/model/Application.java
@@ -11,35 +11,8 @@ import java.util.List;
 
 public class Application {
     private List<User> users;
-    private static List<OutingPlace> places = new ArrayList<>();
-    private static List<OutingEvent> events = new ArrayList<>();
-
-    public static List<IPlanningResult> findOutings(OutingFilter filter) {
-
-        List<IPlanningResult> outingsByTag = new ArrayList<>();
-        for (OutingPlace op : places) {
-            if (op.getTags().contains(filter.getSearchTag())) {
-                outingsByTag.add(op);
-            }
-        }
-
-        List<IPlanningResult> outingsByPrice = new ArrayList<>();
-        for (OutingPlace op : places) {
-            if (op.getPrice() <= filter.getMaxPrice()) {
-                outingsByPrice.add(op);
-            }
-        }
-
-        List<IPlanningResult> outingsByDate = new ArrayList<>();
-        for (OutingPlace op : places) {
-            if (op.getWeekTimeSchedule().includes(filter.getDate(), filter.getTimeSlot())) {
-                outingsByDate.add(op);
-            }
-        }
-
-        return matchOutings(matchOutings(outingsByTag, outingsByPrice), outingsByDate);
-
-    }
+    private static List<OutingPlace> places;
+    private static List<OutingEvent> events;
 
     public List<User> getUsers() {
         return users;
@@ -65,6 +38,55 @@ public class Application {
         this.events = events;
     }
 
+    public static List<IPlanningResult> findOutings(OutingFilter filter) {
+
+        List<IPlanningResult> outingsByTag = findOutingsByTag(filter);
+        List<IPlanningResult> outingsByPrice = findOutingsByPrice(filter);
+        List<IPlanningResult> outingsByDate = findOutingsByDate(filter);
+
+        return matchOutings(matchOutings(outingsByTag, outingsByPrice), outingsByDate);
+
+    }
+
+    private static List<IPlanningResult> findOutingsByDate(OutingFilter filter) {
+        List<IPlanningResult> outingsByDate = new ArrayList<>();
+        for (OutingPlace op : places) {
+            if (op.getWeekTimeSchedule().includes(filter.getDate(), filter.getTimeSlot())) {
+                outingsByDate.add(op);
+            }
+        }
+        return outingsByDate;
+    }
+
+    private static List<IPlanningResult> findOutingsByPrice(OutingFilter filter) {
+        List<IPlanningResult> outingsByPrice = new ArrayList<>();
+        for (OutingPlace op : places) {
+            if (op.getPrice() <= filter.getMaxPrice()) {
+                outingsByPrice.add(op);
+            }
+        }
+        return outingsByPrice;
+    }
+
+    private static List<IPlanningResult> findOutingsByTag(OutingFilter filter) {
+
+        List<IPlanningResult> outingsByTag = new ArrayList<>();
+
+        if (filter.getSearchTag() == null) {
+            /**
+             * TODO
+             */
+            ;
+        }
+
+        for (OutingPlace op : places) {
+            if (op.getTags().contains(filter.getSearchTag())) {
+                outingsByTag.add(op);
+            }
+        }
+        return outingsByTag;
+    }
+
     public static List<IPlanningResult> matchOutings(List<IPlanningResult> listA, List<IPlanningResult> listB) {
         List<IPlanningResult> finalOutings = new ArrayList<>();
         for (IPlanningResult outing : listA) {
@@ -74,4 +96,6 @@ public class Application {
         }
         return finalOutings;
     }
+
+
 }
