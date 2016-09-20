a9cde48a5be53a43f3506b34917a44e3a05020b6
Juan Pablo Camejo <jp.came@gmail.com>
2016-09-20 00:33:36 -0300

    probando deploy frontend

---
+++ desapp-grupoe-frontend/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name adondevamosApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the adondevamosApp
+ */
+angular.module('adondevamosApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
