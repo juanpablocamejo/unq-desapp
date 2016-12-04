ec39ffe2d9416aaa1f95384829206bd10839140d
Esteban <esteban.schafir@gmail.com>
2016-12-04 14:14:17 -0300

    Se agregaron mas events y places junto con sus imagenes y las coordenadas correspondientes para ser utilizadas por Google Maps.

--- desapp-grupoe-backend/src/main/java/services/OutingEventService.java
+++ desapp-grupoe-backend/src/main/java/services/OutingEventService.java
@@ -2,6 +2,8 @@ package services;
 
 import exceptions.EntityValidationException;
 import model.builders.outings.OutingEventBuilder;
+import model.locations.Address;
+import model.locations.Coord;
 import model.outings.OutingEvent;
 import org.springframework.transaction.annotation.Transactional;
 import persistence.AddressDAO;
@@ -25,14 +27,101 @@ public class OutingEventService extends GenericService<OutingEvent> implements I
 
     @Transactional
     public void initialize() throws EntityValidationException {
-        OutingEvent oe1 = OutingEventBuilder.anOutingEvent().withName("Recital Indio Solari").withDescription("Misa India en Tandil").withPrice(800).withMaxAssistants(200000).withImage("https://www.brujulea.net/public/lugares/lugarpkgi58.jpg").build();
-        OutingEvent oe2 = OutingEventBuilder.anOutingEvent().withName("Fiesta Ochentosa").withDescription("Revivi los 80").withPrice(100).withMaxAssistants(200).withImage("https://www.brujulea.net/public/lugares/lugarpkgi58.jpg").build();
-        OutingEvent oe3 = OutingEventBuilder.anOutingEvent().withName("Oktober Fest").withDescription("Birritas!").withPrice(500).withMaxAssistants(300).withImage("https://www.brujulea.net/public/lugares/lugarpkgi58.jpg").build();
-        OutingEvent oe4 = OutingEventBuilder.anOutingEvent().withName("PaintBall").withDescription("Oferta de Paintball para 10 personas").withPrice(80).withMaxAssistants(10).withImage("https://www.brujulea.net/public/lugares/lugarpkgi58.jpg").build();
-        save(oe1);
-        save(oe2);
-        save(oe3);
-        save(oe4);
+        OutingEvent indio = OutingEventBuilder.anOutingEvent()
+                .withName("Recital Indio Solari")
+                .withDescription("Misa India en Tandil")
+                .withPrice(800)
+                .withMaxAssistants(200000)
+                .withImage("http://bucket.rollingstone.com.ar/anexos/fotos/23/2137823.jpg")
+                .withAddress(new Address(new Coord(-37.3021237, -59.1132899), "Tandil"))
+                .build();
+        OutingEvent ochentoso = OutingEventBuilder.anOutingEvent()
+                .withName("Fiesta Ochentosa")
+                .withDescription("Revivi los 80")
+                .withPrice(100)
+                .withMaxAssistants(200)
+                .withImage("https://rlv.zcache.es/fiesta_80s_invitacion_13_3_cm_x_13_3cm-r272ecc5c59fe42e997546cfea2fe72b9_zk9yi_324.jpg?rlvnet=1")
+                .withAddress(new Address(new Coord(-34.585951, -58.434559), "Palermo"))
+                .build();
+        OutingEvent oktober = OutingEventBuilder.anOutingEvent()
+                .withName("Oktober Fest")
+                .withDescription("Birritas!")
+                .withPrice(500)
+                .withMaxAssistants(300)
+                .withImage("http://1.bp.blogspot.com/-23TTolMDie8/VfbPKTgtJMI/AAAAAAAAPOw/TTljk81h11Y/s1600/Oktoberfest-Logo-2015-2.jpg")
+                .withAddress(new Address(new Coord(-34.570165, -58.396094), "Costa Salguero"))
+                .build();
+        OutingEvent paintBall = OutingEventBuilder.anOutingEvent()
+                .withName("PaintBall")
+                .withDescription("Oferta de Paintball para 10 personas")
+                .withPrice(80).withMaxAssistants(10)
+                .withImage("https://i.ytimg.com/vi/r2g4vQR_1i8/maxresdefault.jpg")
+                .withAddress(new Address(new Coord(-34.5343855, -58.4510147), "Ezeiza"))
+                .build();
+        OutingEvent comicCon = OutingEventBuilder.anOutingEvent()
+                .withName("ComicCon 2016")
+                .withDescription("El evento mas esperado del 2016!")
+                .withPrice(1500).withMaxAssistants(500)
+                .withImage("http://www.argentinacomiccon.com.ar/wp-content/uploads/2016/08/logo-preview.png")
+                .withAddress(new Address(new Coord(-34.570165, -58.396094), "Costa Salguero"))
+                .build();
+        OutingEvent gameAwards = OutingEventBuilder.anOutingEvent()
+                .withName("Game Awards 2016")
+                .withDescription("Evento ideal para gamers. Candidato: Overwatch de Blizzard")
+                .withPrice(1000).withMaxAssistants(1000)
+                .withImage("http://sm.ign.com/ign_es/screenshot/default/2016111618176-1_n9hf.jpg")
+                .withAddress(new Address(new Coord(-34.6065844, -58.4105916), "CABA"))
+                .build();
+        OutingEvent chori = OutingEventBuilder.anOutingEvent()
+                .withName("Choripateada UNQ")
+                .withDescription("Pa' despedir el año")
+                .withPrice(50).withMaxAssistants(200)
+                .withImage("https://ugc.kn3.net/i/origin/http://farm9.static.flickr.com/8322/7975156453_f1d168cf0d.jpg")
+                .withAddress(new Address(new Coord(-34.5848217, -58.393176), "Recoleta"))
+                .build();
+        OutingEvent feria = OutingEventBuilder.anOutingEvent()
+                .withName("Feria americana")
+                .withDescription("Comprá todas las boludeces que quieras...")
+                .withPrice(10)
+                .withMaxAssistants(150)
+                .withImage("http://www.baraderoteinforma.com.ar/wp-content/uploads/2016/09/feria.jpg")
+                .withAddress(new Address(new Coord(-34.6277849, -58.3806365), "Constitucion"))
+                .build();
+        OutingEvent partido = OutingEventBuilder.anOutingEvent()
+                .withName("Partido de futbol")
+                .withDescription("Argentina vs Brasil")
+                .withPrice(300)
+                .withMaxAssistants(75000)
+                .withImage("http://toqueygambeta.files.wordpress.com/2011/06/estadio-monumental-copa-america6.jpg")
+                .withAddress(new Address(new Coord(-34.5453062, -58.4497749), "Nuñez"))
+                .build();
+        OutingEvent espuma = OutingEventBuilder.anOutingEvent()
+                .withName("Fiesta de la espuma")
+                .withDescription("Oh si....")
+                .withPrice(200)
+                .withMaxAssistants(400)
+                .withImage("http://leporepropiedades.com.ar/public/lepore/templates/img/sections/barrios/barrio-puerto-madero.jpg")
+                .withAddress(new Address(new Coord(-34.605632, -58.364317), "Puerto Madero"))
+                .build();
+        OutingEvent disfraces = OutingEventBuilder.anOutingEvent()
+                .withName("Fiesta de disfraces")
+                .withDescription("Bizarreada total.. Hay varios premios en juego! No podes faltar.")
+                .withPrice(250).withMaxAssistants(100)
+                .withImage("http://media.informedigital.com.ar//fotos/2011/10/14/o_1318617360.jpg")
+                .withAddress(new Address(new Coord(-34.610465, -58.3636987), "Puerto Madero"))
+                .build();
+
+        save(indio);
+        save(ochentoso);
+        save(oktober);
+        save(paintBall);
+        save(comicCon);
+        save(gameAwards);
+        save(chori);
+        save(feria);
+        save(partido);
+        save(espuma);
+        save(disfraces);
     }
 
     @Override
--- desapp-grupoe-backend/src/main/java/services/OutingPlaceService.java
+++ desapp-grupoe-backend/src/main/java/services/OutingPlaceService.java
@@ -3,6 +3,8 @@ package services;
 import exceptions.EntityValidationException;
 import model.builders.outings.OutingPlaceBuilder;
 import model.builders.time.WeekTimeScheduleBuilder;
+import model.locations.Address;
+import model.locations.Coord;
 import model.outings.OutingPlace;
 import org.springframework.transaction.annotation.Transactional;
 import persistence.AddressDAO;
@@ -33,12 +35,80 @@ public class OutingPlaceService extends GenericService<OutingPlace> implements I
 
     @Transactional
     public void initialize() throws EntityValidationException {
-        OutingPlace cuartetas = OutingPlaceBuilder.anOutingPlace().withName("Las Cuartetas").withDescription("Una de las pizzerias mas populares de Buenos Aires").withPrice(200).withWeekTimeSchedule(WeekTimeScheduleBuilder.anyWeekTimeSchedule().build()).withImage("https://www.brujulea.net/public/lugares/lugarpkgi58.jpg").build();
-        OutingPlace tgiFridays = OutingPlaceBuilder.anOutingPlace().withName("TGI Fridays").withDescription("Restaurant popular de Puerto Madero").withPrice(600).withWeekTimeSchedule(WeekTimeScheduleBuilder.anyWeekTimeSchedule().build()).withImage("https://www.brujulea.net/public/lugares/lugarpkgi58.jpg").build();
-        OutingPlace milanesa = OutingPlaceBuilder.anOutingPlace().withName("El Palacio de la Milanesa").withDescription("Altas milangas").withPrice(150).withWeekTimeSchedule(WeekTimeScheduleBuilder.anyWeekTimeSchedule().build()).withImage("https://www.brujulea.net/public/lugares/lugarpkgi58.jpg").build();
+
+        OutingPlace cuartetas = OutingPlaceBuilder.anOutingPlace()
+                .withName("Las Cuartetas")
+                .withDescription("Una de las pizzerias mas populares de Buenos Aires")
+                .withPrice(200)
+                .withWeekTimeSchedule(WeekTimeScheduleBuilder.anyWeekTimeSchedule().build())
+                .withImage("http://www.lamejorpizzeria.com/wp-content/uploads/2014/12/lascuartetas-1024x768.jpg")
+                .withAddress(new Address(new Coord(-34.60375, -58.3785746), "Buenos Aires"))
+                .build();
+
+        OutingPlace tgiFridays = OutingPlaceBuilder.anOutingPlace()
+                .withName("TGI Fridays")
+                .withDescription("Restaurant popular de Puerto Madero")
+                .withPrice(600)
+                .withWeekTimeSchedule(WeekTimeScheduleBuilder.anyWeekTimeSchedule().build())
+                .withImage("https://www.fluidnetwork.co.uk/gfx/venues/22759/tgi_fridays_restaurant_london_westfield_mall_1.jpg")
+                .withAddress(new Address(new Coord(-34.6095008, -58.3662253), "Buenos Aires"))
+                .build();
+
+        OutingPlace mcdonalds = OutingPlaceBuilder.anOutingPlace()
+                .withName("Mc Donalds")
+                .withDescription("Me encanta!")
+                .withPrice(150)
+                .withWeekTimeSchedule(WeekTimeScheduleBuilder.anyWeekTimeSchedule().build())
+                .withImage("https://yt3.ggpht.com/-avTHbIvvjKY/AAAAAAAAAAI/AAAAAAAAAAA/GtO4B-SrWkA/s900-c-k-no-mo-rj-c0xffffff/photo.jpg")
+                .withAddress(new Address(new Coord(-34.6038946, -58.3807468), "Microcentro"))
+                .build();
+        OutingPlace sigalavaca = OutingPlaceBuilder.anOutingPlace()
+                .withName("Siga la vaca")
+                .withDescription("www.sigalavaca.com").withPrice(100)
+                .withWeekTimeSchedule(WeekTimeScheduleBuilder.anyWeekTimeSchedule().build())
+                .withImage("http://www.ediciona.com/portafolio/image/7/6/0/7/5_siga_la_vaca_7067.jpg")
+                .withAddress(new Address(new Coord(-34.61863, -58.3651501), "Puerto Madero"))
+                .build();
+        OutingPlace cinemark = OutingPlaceBuilder.anOutingPlace()
+                .withName("Cinemark")
+                .withDescription("Cines de Argentina")
+                .withPrice(85)
+                .withWeekTimeSchedule(WeekTimeScheduleBuilder.anyWeekTimeSchedule().build())
+                .withImage("https://lh4.ggpht.com/Ix-Qx3srvC-cRW6xzvHHpaoYhooj-54rfDGbHtIpaXvly2YK28eB4qNgEnIzyVM7zA=w300")
+                .withAddress(new Address(new Coord(-34.5864633, -58.410349), "Recoleta"))
+                .build();
+        OutingPlace colon = OutingPlaceBuilder.anOutingPlace()
+                .withName("Teatro Colon")
+                .withDescription("El teatro mas emblematico de la Argentina")
+                .withPrice(150)
+                .withWeekTimeSchedule(WeekTimeScheduleBuilder.anyWeekTimeSchedule().build())
+                .withImage("http://disfrutemosba.buenosaires.gob.ar/imagenes/imagegallery/ce05-foto-exterior-nocturna-teatro-colon-copia.jpg")
+                .withAddress(new Address(new Coord(-34.6010406, -58.3830786), "Microcentro"))
+                .build();
+        OutingPlace luna = OutingPlaceBuilder.anOutingPlace()
+                .withName("Luna Park")
+                .withDescription("Lugar para todo tipo de shows")
+                .withPrice(150)
+                .withWeekTimeSchedule(WeekTimeScheduleBuilder.anyWeekTimeSchedule().build())
+                .withImage("https://www.brujulea.net/public/lugares/lugarpkgi58.jpg")
+                .withAddress(new Address(new Coord(-34.6023061, -58.3687473), "Retiro"))
+                .build();
+        OutingPlace carrefour = OutingPlaceBuilder.anOutingPlace()
+                .withName("Carrefour")
+                .withDescription("El mejor precio garantizado")
+                .withPrice(150)
+                .withWeekTimeSchedule(WeekTimeScheduleBuilder.anyWeekTimeSchedule().build())
+                .withImage("http://voxpopuli.com.ar/site/wp-content/uploads/2016/04/acaca.png")
+                .withAddress(new Address(new Coord(-34.5888012, -58.4005737), "Recoleta"))
+                .build();
         save(cuartetas);
         save(tgiFridays);
-        save(milanesa);
+        save(mcdonalds);
+        save(sigalavaca);
+        save(cinemark);
+        save(colon);
+        save(luna);
+        save(carrefour);
     }
 
     @Override
